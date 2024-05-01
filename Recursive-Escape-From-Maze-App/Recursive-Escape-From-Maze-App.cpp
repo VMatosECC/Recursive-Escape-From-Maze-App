db6f534f26990c0c1dea22511f3a8fb6786e8c63
VICTOR MATOS <vmatos@elcamino.edu>
2024-05-01 14:42:56 -0700

    Add project files.

---
+++ Recursive-Escape-From-Maze-App/Recursive-Escape-From-Maze-App.cpp
@@ -0,0 +1,98 @@
+// Recursive-Escape-From-Maze-App.cpp 
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//-------------------------------------------------------
+//maze: 'o' (free), 'b' (blocked), 'e' (exit), '*' (visited), 's' (starting point)
+//char maze[4][5]{
+vector<vector<char>> maze
+{
+    {'o', 'e', 'b', 'e', 'o', },
+    {'o', 'b', 's', 'o', 'b', },
+    {'o', 'o', 'o', 'b', 'b', },
+    {'b', 'b', 'o', 'b', 'b', }
+};
+
+const int TROWS = maze.size();
+const int TCOLS = maze[0].size();
+//Prototypes ----------------------------------------
+void showMaze(vector <vector<char>> maze);
+bool escape(vector<vector<char>>& maze, int row, int col, vector<pair<int, int>>& path);
+
+int main()
+{
+
+    showMaze(maze);
+
+    vector<pair<int, int>> path;
+    int startingRow = 1;
+    int startingCol = 2;
+
+    escape(maze, startingRow, startingCol, path);
+
+    if (path.size() == 0)
+    {
+        cout << "No escape path was found\n";
+    }
+    else
+    {
+        cout << "A escape path is: " << endl;
+        for (auto cell : path) {
+            cout << "( " << cell.first << ", " << cell.second << ")" << endl;
+        }
+    }
+    cout << "\nAll done!\n";
+}
+//--------------------------------------------------
+bool escape(vector<vector<char>>& maze, int row, int col, vector<pair<int, int>>& path)
+{
+    //Is the following cell promising?
+    if (row >= TROWS || row < 0 ||
+        col >= TCOLS || col < 0 ||
+        maze[row][col] == '*' || maze[row][col] == 'b')
+    {
+        return false;
+    }
+    
+    //put current cell into the exit path
+    path.emplace_back(row, col);    
+
+    //did we arrived to an exit?
+    if (maze[row][col] == 'e') return true;
+
+    //General recursion - Initiate exploration from current cell
+    maze[row][col] = '*';           //mark cell as visited
+
+    if (escape(maze, row - 1, col, path) ||         //go north
+        escape(maze, row + 1, col, path) ||         //go south
+        escape(maze, row, col + 1, path) ||         //go east
+        escape(maze, row, col - 1, path))            //go west
+    {
+        return true;
+    }
+    else
+    {
+        path.pop_back();
+        return false;
+    }
+
+}
+
+
+
+// -------------------------------------------------
+void showMaze(vector <vector<char>> maze)
+{
+    cout << endl;
+    for (int row = 0; row < TROWS; row++)
+    {
+        for (int col = 0; col < TCOLS; col++)
+        {
+            cout << "\t" << maze[row][col];
+        }
+        cout << endl;
+    }
+}
